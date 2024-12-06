5f923182dcb14ab8558c0b5ac125daea874770a9
Lili Enseling <liliyang@ad.unc.edu>
2024-12-06 15:26:37 -0500

    Added player light and coin is collectable

--- MazeGame/Assets/ContinuousMovement.cs
+++ MazeGame/Assets/ContinuousMovement.cs
@@ -6,12 +6,32 @@ public class ContinuousMovement : MonoBehaviour
     public Transform vrRig; // Reference to the OVRCameraRig (e.g., CenterEyeAnchor)
     private Rigidbody rb;
 
+    public AudioClip CoinSound = null;
+
+    private Rigidbody mRigidBody = null;
+    private AudioSource mAudioSource = null;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
         // Lock rotation to prevent tipping over
         rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+        mRigidBody = GetComponent<Rigidbody>();
+        mAudioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Coin"))
+        {
+            if (mAudioSource != null && CoinSound != null)
+            {
+                mAudioSource.PlayOneShot(CoinSound);
+            }
+            Destroy(other.gameObject);
+        }
     }
 
     void FixedUpdate()
